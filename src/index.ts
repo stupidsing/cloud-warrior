bf8bca68fa16ff17a0a80db3cb64dde4c6e2a0fa
stupidsing <stupidsing@gmail.com>
2024-09-03 05:38:47 +0000

    -

--- src/index.ts
+++ src/index.ts
@@ -43,33 +43,33 @@ run(() => {
 		Roles: [{ RoleName: role.getRoleName(get) }],
 	}));
 
-	let vpc = createVpc('a', get => ({
+	let vpc = createVpc('vpc', get => ({
 		CidrBlockAssociationSet: [{ CidrBlock: '10.88.0.0/16' }],
 		EnableDnsHostnames: true,
 		EnableDnsSupport: true,
 	}));
 
-	let subnetPublic = createSubnet('public', get => ({
+	let subnetPublic = createSubnet('subnet-public', get => ({
 		AvailabilityZone: 'ap-southeast-1a',
 		CidrBlock: '10.88.1.0/24',
 		MapPublicIpOnLaunch: true,
 		VpcId: vpc.getVpcId(get),
 	}));
 
-	let subnetPrivate = createSubnet('private', get => ({
+	let subnetPrivate = createSubnet('subnet-private', get => ({
 		AvailabilityZone: 'ap-southeast-1a',
 		CidrBlock: '10.88.2.0/24',
 		MapPublicIpOnLaunch: false,
 		VpcId: vpc.getVpcId(get),
 	}));
 
-	let securityGroup = createSecurityGroup('app', get => ({
+	let securityGroup = createSecurityGroup('sg-app', get => ({
 		Description: '-',
 		GroupName: `${prefix}-sg-app`,
 		VpcId: vpc.getVpcId(get),
 	}));
 
-	let instance = createInstance('0', get => ({
+	let instance = createInstance('instance-0', get => ({
 		IamInstanceProfile: { Arn: instanceProfile.getArn(get) },
 		ImageId: 'ami-05d6d0aae066c8d93', // aws ssm get-parameter --name /aws/service/canonical/ubuntu/server/24.04/stable/current/amd64/hvm/ebs-gp3/ami-id | jq -r .Parameter.Value
 		InstanceType: 't3.nano',
